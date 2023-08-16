d5071285c107f6e0b8b22379a67e4ed927217d83
adarsh-2425 <adarsh.lol2425@gmail.com>
2023-08-16 11:44:58 +0000

    otp ok

--- routes/otpRoute.js
+++ routes/otpRoute.js
@@ -1,15 +1,36 @@
 const express = require("express");
-const { sendOTPByEmail } = require("../middlewares/otpMiddleware");
-const otpGenerator  = require("../utils/otpGenerator");
+const User = require("../models/User");
+const { compareOTP } = require("../utils/otpGenerator");
 
 const router = express.Router();
 
-router.post("/send-otp", async (req, res) => {
-  const { email } = req.body;
-  const otp = otpGenerator.generateOTP();
+//verify otp
+router.post("/verify-otp", async (req, res) => {
+  const { email, otp } = req.body;
 
-  sendOTPByEmail(email, otp);
-  res.json({message: "OTP sent successfully"});
+  try {
+    // Find user by email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Compare received OTP with stored OTP
+    if (compareOTP(otp, user.otp)) {
+      // Update verified field to true
+      user.verified = true;
+      user.otp = null; // Set the OTP field to null after verification
+      await user.save();
+
+      return res.json({ message: "OTP verified successfully" });
+    } else {
+      return res.status(401).json({ message: "Invalid OTP" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
 });
 
 module.exports = router;
--- utils/otpGenerator.js
+++ utils/otpGenerator.js
@@ -1,8 +1,14 @@
 const randomstring = require("randomstring");
 
+// Generate a random OTP
 exports.generateOTP = () => {
   return randomstring.generate({
     length: 6,
     charset: "numeric"
   })
+}
+
+// Compare OTPs
+exports.compareOTP = (receivedOTP, storedOTP) => {
+  return receivedOTP === storedOTP;
 }
